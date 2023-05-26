b758378137407e2cc5c253a20581dbf97604a434
devdouglasleite <devdouglasleite@gmail.com>
2023-05-26 00:57:23 -0300

    realizado

---
+++ main.js
@@ -0,0 +1,11 @@
+function generatePassword() {
+    var length = 10;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var password = "";
+
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    document.getElementById("passwordField").value = password;
+}
